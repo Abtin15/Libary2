a638be02a262023f74cec832a2743749cb262f66
Abtin <abtin_nasiri2010@yahoo.com>
2025-06-07 19:49:48 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
-
+    static Admin admin = new Admin("admin", "1234");
     public static void main(String[] args) {
-
+        showMenu();
     }
 
     public static void showMenu() {
@@ -18,14 +18,46 @@ public class Main {
             option = scanner.nextInt();
         } while (option > 3 || option < 1);
         switch (option) {
-
-            }
+            case 1 -> loginAdminMenu();
+        }
         // TODO: add admin menu method
-        
+
         // TODO: add user menu method
     }
-    public static void adminMenu() {
-        System.out.println(" * * * ADMIN * * * ");
-
+// TODO: rename this method to loginAdminMenu
+    public static void loginAdminMenu() {
+        System.out.println(" * * * ADMIN MENU * * * ");
+        scanner.nextLine();
+        System.out.print("Enter username : ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password : ");
+        String password = scanner.nextLine();
+//TODO: if username and password is correct go to adminMenu else print error and go to main menu
+        if (username.equals(admin.getUsername()) && password.equals(admin.getPassword())) {
+            System.out.println("Login Successful");
+            showAdminMenu();
+        } else {
+            System.out.println("Invalid username or password");
+            showMenu();
+        }
+    }
+    //TODO: refactor this method with userMenu like the PDF 
+    public static void userMenu() {
+        System.out.println(" * * * USER MENU * * * ");
+        scanner.nextLine();
+            System.out.print("Enter username : ");
+            String username = scanner.nextLine();
+            System.out.print("Enter password : ");
+            String password = scanner.nextLine();
+            System.out.print("Enter your name");
+            String name = scanner.nextLine();
+            System.out.print("Enter your last name : ");
+            String family = scanner.nextLine();
+            System.out.print(" Enter your age : ");
+            int age = scanner.nextInt();
+            System.out.print("Enter your address : ");
+            String address = scanner.nextLine();
+            System.out.print("Enter your Gender : ");
+            String gender = scanner.nextLine();
     }
 }
